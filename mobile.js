a6f1c30472c20b20a8e7161f50e22977b48c801c
Alan Jimenez <stajimenez97@gmai.com>
2021-09-02 08:19:47 -0700

    Test mobile menu

--- mobile.js
+++ mobile.js
@@ -1,8 +1,16 @@
 `use strict`;
 const hamburger = document.querySelector(`#ham-menu`);
 const menu = document.querySelector(`#mobile-menu`);
+const title = document.querySelectorAll(`.title`);
+const links = document.querySelectorAll(`.links-hidden`);
 
 hamburger.addEventListener(`click`, function () {
   menu.classList.toggle(`display`);
   console.log(`Hello`);
 });
+
+for (let i = 0; i < title.length; i++) {
+  title[i].addEventListener(`click`, function () {
+    links[i].classList.toggle(`display`);
+  });
+}
